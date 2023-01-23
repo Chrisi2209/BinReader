8ee43d99a05183b75c51851a4c8d8a626aa6e078
Chrisi2209 <christopher.rodax@gmail.com>
2023-01-23 11:20:21 +0100

    done

---
+++ src/main.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+def show_binary():
+    # if no path 
+    try:
+        bin_file = sys.argv[1]
+    except(IndexError):
+        # if there is no path, error
+        print("No path was given.")
+        return
+
+    # function to convert the bytes object into a string
+    # if it is hex, a hex representation will be printed
+    # if it is bin, a bin representation will be printed
+    base_func = hex
+    number_zfill = 2  # how many digits each number has
+    if len(sys.argv) == 3:
+        if sys.argv[2] == "bin":
+            base_func = bin
+            number_zfill = 8
+        elif sys.argv[2] == "dec":
+            base_func = lambda x: "0d" + str(x)
+            number_zfill = 3
+        elif sys.argv[2] == "hex":
+            base_func = hex
+            number_zfill = 2
+        else:
+            print("Invalid arguments")
+            return
+    
+    
+    with open(bin_file, "rb") as f:
+        for line in f:
+            for byte in line:
+                # get the bin/hex string of the byte
+                binary_string = base_func(byte)[2:].zfill(number_zfill)
+
+                # the binary file is printed out
+                print(binary_string, end=" ")
+            # go to next line after line ends
+            print()
+
+
+if __name__ == "__main__":
+    show_binary()
